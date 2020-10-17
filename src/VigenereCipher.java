3ecdd576b22350c4e625f424be0db53b2a6811e1
Ziv <zivfreud@gmail.com>
2020-10-17 17:12:47 +0300

    Added comments, cleaned the code and fixed a bug in the case of a capital letter in the encrypt function

--- src/VigenereCipher.java
+++ src/VigenereCipher.java
@@ -5,12 +5,17 @@ public class VigenereCipher {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
 
+        //Getting user input
         System.out.println("Please enter the text: ");
         String input = scanner.nextLine();
+
         String key;
 
+
+        //Getting user action
         boolean isEncrypting = false;
         String choice = "";
+        //The 2 only actions are encrypt and decrypt. The user wont continue until he choose one of them
         while (!(choice.equals("encrypt") || choice.equals("decrypt"))) {
             System.out.println("Do you want to encrypt or decrypt?");
             choice = (scanner.nextLine()).toLowerCase();
@@ -27,6 +32,7 @@ public class VigenereCipher {
 
         String output;
 
+        //Performing the given action
         if (isEncrypting)
             output = encrypt(input, key);
         else
@@ -38,21 +44,24 @@ public class VigenereCipher {
 
     public static String encrypt(String str, String key)
     {
+        //Adding 'Z' to the string if it length is shorter than the key
         for (int i = str.length(); i < key.length(); i++){
             str += "Z";
         }
         String output = "";
 
         int j = 0;
+        // j is the index for the key
+        // and i is the index for the str
+        // note that j doesn't change when a special letter is obtained
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == ' ') {
-                output += " ";
+            //Special letter case. the encryption skips this kind of letters
+            if (str.charAt(i) == ' ' || str.charAt(i) == '.' || str.charAt(i) == '!' || str.charAt(i) == ','
+                || str.charAt(i) == '(' || str.charAt(i) == ')') {
+                output += str.charAt(i);
             }
-            else if (str.charAt(i) == '.')
-                output += ".";
-            else if (str.charAt(i) == '!')
-                output += "!";
             else {
+                //Checking if the letter is a capital letter
                 boolean isCapital;
                 if (str.charAt(i) >= 65 && str.charAt(i) <= 90)
                     isCapital = true;
@@ -61,8 +70,10 @@ public class VigenereCipher {
                 else
                     return "Unsupported letter error: " + str.charAt(i);
 
-                output += isCapital ? (char) ((str.charAt(i) + key.charAt(j % key.length()) - 2 * 'A') % 26 + 'A')
-                        : (char)((str.charAt(i) + key.charAt(j % key.length()) - 2 * 'a') % 26 + 'a');
+                //Adding the key to the string in the current letter
+                output += isCapital ? (char) ((str.charAt(i) + key.charAt(j % key.length()) - 'A' -'a') % 26 + 'A')
+                        :(char)((str.charAt(i) + key.charAt(j % key.length()) - 2 * 'a') % 26 + 'a');
+                //Note that we uses key.charAt(j % key.length()) in order to repeat the key if it's shorter than the string
                 j++;
             }
         }
@@ -71,29 +82,41 @@ public class VigenereCipher {
     }
     public static String decrypt(String str, String key)
     {
-
+        //Adding 'Z' to the string if it length is shorter than the key
         for (int i = str.length(); i < key.length(); i++){
             str += "Z";
         }
         String output = "";
         int j = 0;
+        // j is the index for the key
+        // and i is the index for the str
+        // note that j doesn't change when a special letter is obtained
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == ' ')
-                output += " ";
-            else if (str.charAt(i) == '.')
-                output += ".";
-            else if (str.charAt(i) == '!')
-                output += "!";
+            //Special letter case. the encryption skips this kind of letters
+            if (str.charAt(i) == ' ' || str.charAt(i) == '.' || str.charAt(i) == '!'|| str.charAt(i) == ','
+                    || str.charAt(i) == '(' || str.charAt(i) == ')') {
+                output += str.charAt(i);
+            }
             else {
-                int difference = (str.charAt(i)+"").toLowerCase().charAt(0) - (key.charAt(j % key.length())+"").toLowerCase().charAt(0);
                 boolean isCapital;
+                //Checking if the letter is a capital letter
                 if (str.charAt(i) >= 65 && str.charAt(i) <= 90)
                     isCapital = true;
                 else if (str.charAt(i) >= 97 && str.charAt(i) <= 122)
                     isCapital = false;
                 else
                     return "Unsupported letter error: " + str.charAt(i);
-                if (difference < 0) {
+                int difference = (str.charAt(i)+"").toLowerCase().charAt(0) - (key.charAt(j % key.length())+"").toLowerCase().charAt(0);
+                //If we subtract a letter that is larger than the current letter
+                //we want to continue backwards from z back to a
+
+                //The (str.charAt(index)+"").toLowerCase().charAt(0) is a tricky way of using toLowerCase
+                //on chars. we convert it to string by + "" then we use toLowerCase function
+                //and then because we only have one letter we can convert it back to char
+                // using charAt(0)
+
+
+                if (difference < 0) { //The case were we go backwards from z to a
                     output += isCapital ? (char) ('Z' + difference + 1) : (char) ('z' + difference + 1);
                 } else
                     output += isCapital ? (char) (difference + 'A') : (char) (difference + 'a');
